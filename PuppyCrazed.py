8c6e281076c86565acb9386a0fc58005aebf9ead
Katie Simmons <annihilatrix@users.noreply.github.com>
2016-06-17 19:04:35 -0700

    Add files via upload

--- PuppyCrazed.py
+++ PuppyCrazed.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
+from model import View, User
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/matches')
+def match():
+    return render_template("match.html")
 
 
 if __name__ == '__main__':
---
+++ seed.py
@@ -0,0 +1,8 @@
+from sqlalchemy import func
+from model import Puppies
+
+from model import connect_to_db, db
+from server import app
+
+# for image in image folder
+    #
